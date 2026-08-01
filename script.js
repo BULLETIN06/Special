4c832837fca94bd6d401619d740327f6981cdd6f
BULLETIN06 <nerrickthiaka@gmail.com>
2026-08-01 12:31:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
    Written & Directed by Engineer Nerrick
 ========================================== */
 
+const music = document.getElementById("music");
+
+window.addEventListener("click", () => {
+    music.play().catch(error => console.log(error));
+}, { once: true });
 // Romantic background music
 const music = document.getElementById("music");
 
